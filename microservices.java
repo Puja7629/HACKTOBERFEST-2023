3a4a46874210400efc42ccb629822114eb0f0afb
Puja Kumari <puja.hit01@gmail.com>
2023-10-05 21:44:46 +0530

    Create microservices.java

---
+++ microservices.java
@@ -0,0 +1,9 @@
+// MicroserviceMain.java
+
+public class MicroserviceMain {
+
+    public static void main(String[] args) {
+        // Your microservice initialization code here
+        System.out.println("Microservice is running...");
+    }
+}
